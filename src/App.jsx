266e0ebe068a36d0bdf7fcb0f4c6fb4f614f9cc3
sdangelo-finomnia <silvestro.dangelo@finomnia.com>
2026-06-15 10:07:18 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
+import AboutUs from "./components/AboutUs";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="landing">
+              <h1>Paradise Nursery</h1>
+              <p>Your plant paradise</p>
+              <Link to="/products">
+                <button>Get Started</button>
+              </Link>
+            </div>
+          }
+        />
+
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+``
