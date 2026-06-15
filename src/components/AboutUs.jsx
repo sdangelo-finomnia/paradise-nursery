b24ad17aa17b3dc957fbd593644f96fe08d6be27
sdangelo-finomnia <silvestro.dangelo@finomnia.com>
2026-06-15 10:05:45 +0200

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>
+        We bring nature closer to your home with beautiful indoor plants.
+      </p>
+    </div>
+  );
+}
